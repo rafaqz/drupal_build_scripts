7ef1bb606351b47b2aa9359270204a0f9a9bad8c
Rafael Schouten <rafaelschouten@gmail.com>
2014-01-04 12:10:48 +1100

    features dir permissions

--- install.sh
+++ install.sh
@@ -64,6 +64,9 @@ if cd $BASE_DIR; then
 
     # Set ownership of all files and directories.
     sudo chown -R $USER:$GROUP $BASE_DIR/*
+    # Set permissions on features dir. @TODO make this only for dev.
+    sudo chmod 775 $DRUPAL_DIR/profiles/$PROFILES/module/features*
+    sudo chmod 775 $DRUPAL_DIR/sites/all/module/features*
 
     # Run drush commands to enable extra site modules and theme.
     cd $DRUPAL_DIR
