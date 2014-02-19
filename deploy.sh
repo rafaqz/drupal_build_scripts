70d05b1d0df890df9bd8b1054b59bdbf3f3f7ecb
Rafael Schouten <rafaelschouten@gmail.com>
2014-02-19 17:15:19 +1100

    update docs

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 
 # Get current directory and import config and shared functions files.
 SCRIPT_DIR=`dirname $0`
 source "$SCRIPT_DIR/config.sh"
 source "$SCRIPT_DIR/functions.sh"
 
+# Run supplied command.
 eval "$@"
