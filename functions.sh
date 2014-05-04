fbab754225d2338728da9939a7dae48e6b7bf1cf
Rafael Schouten <rafaelschouten@gmail.com>
2014-05-04 23:12:27 +0200

    add line for formatting

--- functions.sh
+++ functions.sh
@@ -146,6 +146,7 @@ add_settings() {
   settings=$(wget -qO- $EXTRA_SETTINGS)
   cat >> $new_instance_dir/sites/default/settings.php <<< $settings
 }
+
 repair_tables() {
   # Fixe ids in tables that use only id without a machine name.
   dep set_new_alias
